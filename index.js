98ddf8f04343897bf72efccd682ba8a1af27808c
bartekcostam <bartekoffice1@gmail.com>
2020-03-22 19:45:56 +0000

    food random location

--- index.js
+++ index.js
@@ -13,20 +13,44 @@ let ground = new Image()
 
 ground.src = './img/ground.png'
 
-let food = new Image()
+let foodImg = new Image()
 
-food.src = './img/food.png'
+foodImg.src = './img/food.png'
+
+// create snake
+
+let snake = []
+
+snake[0] = {
+  x: 9 * box,
+  y: 10 * box,
+}
+
+// create food
+let food = {
+  x: Math.floor(Math.random() * 17 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 3) * box,
+}
+
+//create the score var
+
+let score = 0
+
+//drow everythung to the canvas
+
+function draw() {
+  ctx.drawImage(ground, 0, 0)
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? 'green' : 'white'
+    ctx.fillRect(snake[i].x, snake[i].y, box, box)
+
+    ctx.strokeStyle = 'red'
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box)
+  }
+  ctx.drawImage(foodImg, food.x, food.y)
+}
+//call draw function every 100ms
+
+let game = setInterval(draw, 100)
 //load audio
-
-let audioName = new Audio()
-
-audioName.src = './audio'
-
-audioName.play()
-
-ctx.drawImage(imageName, X, Y, Width, Height)
-
-ctx.drawImage()
-
-ctx.fillStyle = 'red'
-ctx.fillRect(100, 300, 30, 30)
