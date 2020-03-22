cd134e0f08eec3eb7d43943feaab681018ea8a7f
bartekcostam <bartekoffice1@gmail.com>
2020-03-22 19:57:51 +0000

    contol snake movment

--- index.js
+++ index.js
@@ -36,6 +36,23 @@ let food = {
 
 let score = 0
 
+//control the snake
+let d
+
+document.addEventListener('keydown', direction)
+
+function direction(event) {
+  if (event.keyCode == 37) {
+    d = 'LEFT'
+  } else if (event.keyCode == 38) {
+    d = 'UP'
+  } else if (event.keyCode == 39) {
+    d = 'RIGHT'
+  } else if (event.keyCode == 40) {
+    d = 'DOWN'
+  }
+}
+
 //drow everythung to the canvas
 
 function draw() {
@@ -49,6 +66,32 @@ function draw() {
     ctx.strokeRect(snake[i].x, snake[i].y, box, box)
   }
   ctx.drawImage(foodImg, food.x, food.y)
+
+  //old head position
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  //remove the tail
+  snake.pop()
+
+  //which direction
+  if (d == 'LEFT') snakeX -= box
+  if (d == 'UP') snakeY -= box
+  if (d == 'RIGHT') snakeX += box
+  if (d == 'DOWN') snakeY += box
+
+  //add new Head
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  }
+  snake.unshift(newHead)
+
+  ctx.fillStyle = 'white'
+  ctx.font = '45px Changa one'
+  ctx.fillText(score, 2 * box, 1.6 * box)
 }
 //call draw function every 100ms
 
