008fc87005a1552000e2762c88fe5e69c2d7b1da
bartekcostam <bartekoffice1@gmail.com>
2020-03-22 19:32:22 +0000

    creating ground and basic elements

--- index.js
+++ index.js
@@ -2,4 +2,31 @@ const cvs = document.getElementById('canvas')
 
 const ctx = cvs.getContext('2d')
 
+//create unit
+
+const box = 32
+
+//
+
 //load image
+let ground = new Image()
+
+ground.src = './img/ground.png'
+
+let food = new Image()
+
+food.src = './img/food.png'
+//load audio
+
+let audioName = new Audio()
+
+audioName.src = './audio'
+
+audioName.play()
+
+ctx.drawImage(imageName, X, Y, Width, Height)
+
+ctx.drawImage()
+
+ctx.fillStyle = 'red'
+ctx.fillRect(100, 300, 30, 30)
